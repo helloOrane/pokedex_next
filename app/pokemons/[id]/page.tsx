776b72ca38ce6e4244bbebdf0c6753fe4a0129a3
Orane Nelson <cenelson.orane@gmail.com>
2024-10-03 16:18:01 +0200

    improve responsive

--- app/pokemons/[id]/page.tsx
+++ app/pokemons/[id]/page.tsx
@@ -13,30 +13,33 @@ export default async function Page({ params }: { params: { id: string } }) {
 
   return (
     <>
-        <div className="flex flex-col gap-4">
-            <div className="">
-              <Suspense fallback={<p>Loading...</p>}>
-                  <PokemonCard pokemon={pokemon} />
-              </Suspense>
-            </div>
-            <div className="flex flex-row gap-4">
-              <div className="grow">
+        <div className="flex flex-col gap-4 lg:flex-row">
+            <div className="lg:w-1/2 flex flex-col gap-4">
+              <div className="">
                 <Suspense fallback={<p>Loading...</p>}>
-                    <TypeList pokemon={pokemon} />
+                    <PokemonCard pokemon={pokemon} />
                 </Suspense>
               </div>
-              <div className="grow">
-                <Suspense fallback={<p>Loading...</p>}>
-                  { pokemon != null && (<Weakness pokemon={pokemon} />)}           
-                </Suspense>
-              </div>
-            </div>
-            <div className="col-span-2 md:row-span-2 md:grid-rows-2">
-              {pokemon != null && Boolean(pokemon.can_evolve) && (
+              <div className="flex flex-row gap-4">
+                <div className="grow">
                   <Suspense fallback={<p>Loading...</p>}>
-                      <Evolution pokemon={{ ...pokemon, name: pokemon.name ?? '' }} />
+                      <TypeList pokemon={pokemon} />
                   </Suspense>
-              )}
+                </div>
+                <div className="grow">
+                  <Suspense fallback={<p>Loading...</p>}>
+                    { pokemon != null && (<Weakness pokemon={pokemon} />)}           
+                  </Suspense>
+                </div>
+              </div>
+
+            </div>
+            <div className="flex gap-4 lg:1/2">
+                {pokemon != null && Boolean(pokemon.can_evolve) && (
+                    <Suspense fallback={<p>Loading...</p>}>
+                        <Evolution pokemon={{ ...pokemon, name: pokemon.name ?? '' }} />
+                    </Suspense>
+                )}
             </div>
         </div>
         <div>
